baa158d8525ef26e2199177b54c6a60e91eb0fbc
Dino Srdoč <dsrdoc96@gmail.com>
2018-05-03 11:36:30 +0200

    test intersection with graphs

---
+++ Intersections/Intersection.swift
@@ -0,0 +1,19 @@
+//
+//  Intersection.swift
+//  Intersections
+//
+//  Created by Dino Srdoč on 30/04/2018.
+//  Copyright © 2018 Dino Srdoč. All rights reserved.
+//
+
+import Foundation
+import SwiftGraph
+
+struct Intersection {
+    var graph: WeightedGraph<Int, Int>
+    
+}
+
+class TrafficSignCollectionViewCell: UICollectionViewCell {
+    
+}
--- Intersections/ViewController.swift
+++ Intersections/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import SwiftGraph
 
 extension CGRect {
     var midPoint: CGPoint {
@@ -14,38 +15,116 @@ extension CGRect {
     }
 }
 
-class ViewController: UIViewController {
+@IBDesignable
+class CarView: UIView {
+    @IBInspectable
+    var color: UIColor = .red
     
-    lazy var carLayer: CAShapeLayer = {
-        let baseLayer = CAShapeLayer()
-        let windshieldLayer = CAShapeLayer()
-        let rearWindowLayer = CAShapeLayer()
-        let leftRearviewMirrorLayer = CAShapeLayer()
-        let leftGlassLayer = CAShapeLayer()
+    override func draw(_ rect: CGRect) {
+        super.draw(rect)
         
-        let baseShape = UIBezierPath()
-        baseShape.move(to: CGPoint(x: 0, y: 200))
-        baseShape.addLine(to: CGPoint(x: 0, y: 0))
-        baseShape.addLine(to: CGPoint(x: 50, y: 0))
-        baseShape.addLine(to: CGPoint(x: 50, y: 200))
-        baseShape.close()
-        baseLayer.path = baseShape.cgPath
-        baseLayer.fillColor = UIColor.gray.cgColor
-        baseLayer.strokeColor = UIColor.red.cgColor
-        baseLayer.lineWidth = 2
-        baseLayer.bounds = baseShape.cgPath.boundingBox
+        let rect = UIBezierPath(roundedRect: bounds, byRoundingCorners: UIRectCorner.topLeft, cornerRadii: bounds.size)
+        color.setFill()
+        rect.fill()
         
-        return baseLayer
-    }()
+    }
+}
 
+@IBDesignable
+class RoadView: UIView {
+    @IBInspectable
+    var color: UIColor = .black
+    
+    @IBInspectable
+    var laneWidth: CGFloat = 10
+    
+    @IBInspectable
+    var arrowOffset: CGSize = CGSize(width: 0, height: 20)
+    
+    override func draw(_ rect: CGRect) {
+        super.draw(rect)
+        
+        let rect = UIBezierPath(rect: bounds)
+        let arrow = UIBezierPath()
+        
+        var referencePointArrow = bounds.midPoint
+        referencePointArrow.x += arrowOffset.width
+        referencePointArrow.y += arrowOffset.height
+        
+        arrow.move(to: CGPoint(x: referencePointArrow.x - 3, y: referencePointArrow.y + 10))
+        arrow.addLine(to: CGPoint(x: referencePointArrow.x - 3, y: referencePointArrow.y))
+        arrow.addLine(to: CGPoint(x: referencePointArrow.x - 7, y: referencePointArrow.y))
+        arrow.addLine(to: CGPoint(x: referencePointArrow.x, y: referencePointArrow.y - 5))
+        arrow.addLine(to: CGPoint(x: referencePointArrow.x + 7, y: referencePointArrow.y))
+        arrow.addLine(to: CGPoint(x: referencePointArrow.x + 3, y: referencePointArrow.y))
+        arrow.addLine(to: CGPoint(x: referencePointArrow.x + 3, y: referencePointArrow.y + 10))
+        arrow.close()
+        
+        color.setFill()
+        rect.fill()
+        UIColor.white.setFill()
+        arrow.fill()
+    }
+}
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var grassView: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
         
-        carLayer.position = view.bounds.midPoint
-        view.layer.insertSublayer(carLayer, at: 1)
+        //
+        // A -> D, E -> H => glavna cesta
+        // K -> I, J -> L => sporedna cesta
+        // J -> C, K -> G => weight = 3
+        // svi ostali imaju weight = 1
+        //
+        //
+        //            A     H
+        //            |     ^
+        //            |     |
+        //            ˇ     |
+        //  I<--------B<----G<-----K
+        //            |     ^
+        //            |     |
+        //            ˇ     |
+        //  J-------->C---->F---->L
+        //            |     ^
+        //            |     |
+        //            ˇ     |
+        //            D     E
+        //
+        
+        let intersection = WeightedGraph<String, Int>(vertices: ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L"])
+        
+        let edgePairs = [
+            ("A", "B"), ("B", "C"), ("C", "D"),
+            ("E", "F"), ("F", "G"), ("G", "H"),
+            ("K", "G"), ("G", "B"), ("B", "I"),
+            ("J", "C"), ("C", "F"), ("F", "L")
+        ]
+        let edges = makeEdges(in: intersection, with: edgePairs)
+        edges[6].weight = 3
+        edges[9].weight = 3
+        edges.forEach { intersection.addEdge($0) }
+        graphCost(intersection, from: "K", to: "G")
+        
+        print(intersection)
         
         print("hi there")
+    }
+    
+    func makeEdges(in graph: WeightedGraph<String, Int>, with indices: [(String, String)]) -> [WeightedEdge<Int>] {
+        return indices.map {
+            WeightedEdge<Int>(u: graph.indexOfVertex($0)!,
+                              v: graph.indexOfVertex($1)!,
+                              directed: true,
+                              weight: 1)
+        }
+    }
+    
+    func graphCost(_ graph: WeightedGraph<String, Int>, from source: String, to destination: String) {
+        print(graph.edgeExists(from: source, to: destination))
     }
 
     override func didReceiveMemoryWarning() {
@@ -55,4 +134,3 @@ class ViewController: UIViewController {
 
 
 }
-
